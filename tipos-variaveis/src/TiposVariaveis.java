40804df4432647a7c5a5066ad4c8ffb739944461
PedroMendes22 <pedrohenriquemendess2019@gmail.com>
2023-03-20 14:37:00 -0300

    consertando-duplavariaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salarioMinimo = 2500; //R$2.500
-        double salarioMinimo = 2.500; //R$2,50 
+        double salarioMinimoAlternativo = 2.500; //R$2,50 
     }
 }
 //teste
