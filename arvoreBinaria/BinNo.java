96e2d62f99f2238ad5f24a01e9b46af55899aa72
PedroMendes22 <pedrohenriquemendess2019@gmail.com>
2023-04-27 10:31:02 -0300

    finalizando ArvireBinaria

---
+++ arvoreBinaria/BinNo.java
@@ -0,0 +1,36 @@
+public class BinNo<T extends Comparable<T>>{ //exige que o objeto sgia uma dependencia, todo obj que entrar nese no será obrighado a egui o metodo Comparable
+
+    private T conteudo;
+    private BinNo<T> noEsq;
+    private BinNo<T> noDir;
+    
+    public BinNo(T conteudo){
+        this.conteudo = conteudo;
+        noEsq = noDir = null;
+    }
+    
+    public T getConteudo(){
+        return conteudo;
+    }
+    public void setConteudo(T conteudo){
+        this.conteudo = conteudo;
+    }
+    public BinNo<T> getNoEsq(){
+        return noEsq;
+    }
+
+    public void setNoEsq(BinNo<T> noEsq){
+        this.noEsq=noEsq;
+    }
+    public BinNo<T> getNoDir(){
+        return noDir;
+    }
+    public void setNoDir(BinNo<T> noDir){
+        this.noDir=noDir;
+    }
+
+    @Override
+    public String toString(){
+        return "BinNo{" + "conteudo=" + conteudo + '}';
+    }
+}
